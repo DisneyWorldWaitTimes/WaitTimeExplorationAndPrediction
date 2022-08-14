7eca9aa8dd7e3398421483cf478c6a419a3fb344
kendalldyke14 <kendalldyke14@gmail.com>
2022-08-13 21:37:19 -0500

    cleaning up repo and updating requirements.txt

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
 setup(
     name='src',
-    packages=find_packages(),
     version='0.1.0',
     description='Analysis & Predictive Exploration of Disney World Magic Kingdom Ride Wait Times for 2015-2021',
     author='Kendall Dyke',
